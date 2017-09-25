3b3c3c2d09c3f494c78b770b0026a0af6bb6c6cb
Matheus_Mayron <matheusmayron8@gmail.com>
2017-09-25 07:49:12 -0300

    Projeto criado.

---
+++ app/src/main/java/br/com/mayron/matheus/armrobotcontrol/ArmControlActivity.java
@@ -0,0 +1,13 @@
+package br.com.mayron.matheus.armrobotcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArmControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arm_control);
+    }
+}
